24d6b3dde7f6114505689c74519226d42bcea7d0
SingleCorner <lmllmlllmllllm@gmail.com>
2016-01-07 16:57:19 +0800

    update_help

--- common/views.py
+++ common/views.py
@@ -22,30 +22,32 @@ def cuser(request):
     MsgType = xml.find('MsgType').text
     if MsgType == "text":      
       Request = xml.find('Content').text
-      command = Request.split(' ')[0]
+    else if MsgType == "event":
+      Request = "welcome"
     else:
-      Request = ""
-      command = ""
+      Request = "unsupport"
+
     if Request == "test":
       Message = "真正的秒到账pos，养卡套现神器/::B/::B/::B有需要电话联系哦/::B15821303362"
+    else if Request == "?"
+      Message = "目前支持的命令\n[1]bind account password\n\n[2]group list"
+    else if Request == "welcome":
+      Message = "欢迎关注本公众号\n目前已对接运维管理系统\nhttp://ops.siner.us\n帮助请输入 ?"
+    else if Request == "unsupport"
+      Message = "暂时不支持，有问题联系微信CSZ9227"
     else:
-      if command == "group":
-        req_url = "http://ops.siner.us/api"
-        req_data = urllib.urlencode({'wechat_key':toUser, 'request':Request, 'command':command}) 
-        req = urllib2.Request(url = req_url,data = req_data)
-        res = urllib2.urlopen(req).read()
-        res_data = json.loads(res)
-        if res_data['code'] == 1:
-          Message = res_data['message']
-        else:
-          i = 0
-          Message = ""
-          for msg in res_data['group']:
-            Message = Message + res_data['group'][msg] + "\n"
+      req_url = "http://ops.siner.us/api"
+      req_data = urllib.urlencode({'wechat_key':toUser, 'request':Request}) 
+      req = urllib2.Request(url = req_url,data = req_data)
+      res = urllib2.urlopen(req).read()
+      res_data = json.loads(res)
+      if res_data['code'] == 1:
+        Message = res_data['message']
       else:
-        Message = Request
-    if MsgType == "event":
-      Message = "欢迎关注单边角落\n目前已对接http://ops.siner.us\n支持命令\nbind 用户名 密码\ngroup list"
+        i = 0
+        Message = ""
+        for msg in res_data['group']:
+          Message = Message + res_data['group'][msg] + "\n"
     response = render(request, "text.xml", locals())
   except:
     Message = "test"
